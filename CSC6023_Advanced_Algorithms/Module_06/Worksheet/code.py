2ccd2c6f66479e9584ab3fbe02d25add4a43acc4
baynhamch <130159962+baynhamch@users.noreply.github.com>
2025-02-20 13:42:43 -0500

    Update code.py

--- CSC6023_Advanced_Algorithms/Module_06/Worksheet/code.py
+++ CSC6023_Advanced_Algorithms/Module_06/Worksheet/code.py
@@ -1,2 +1,8 @@
-def las_vegas():
+import random
+
+def las_vegas(target):
+  i = random.randint(0, len(self.nums) - 1)
+        while self.nums[i] != target:
+            i = random.randint(0, len(self.nums) - 1)
+        return i
   
